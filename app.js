7e23f5fb696f5dd2a61d00c4e8afd52c38e2a54a
bbrungardt02 <122824472+bbrungardt02@users.noreply.github.com>
2023-03-01 01:25:18 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
 const newGameButton = document.getElementById("newGameButton");
-const newItemForm = document.querySelector("#new-item");
-const newItemTextInput = document.querySelector("#new-item-text");
 const startGameButton = document.getElementById("startGameButton");
 
 function enterName() {
-  const enterPlayerName = document.getElementById("enterPlayerName");
   const html = `
   <form id="new-item">
     <input id="new-item-text" type="text" placeholder="Enter Player Name" required>
     <button>Submit</button>
   </form>`;
+  const enterPlayerName = document.getElementById("enterPlayerName");
   enterPlayerName.innerHTML = html;
+  function submitName(event) {
+    event.preventDefault();
+    const playerName = document.getElementById("playerName");
+    const newItemTextInput = document.getElementById("new-item-text");
+    playerName.innerText = newItemTextInput.value;
+    newItemForm.style.display = "none";
+    const html2 = `<div class="board">
+  <div class="box"></div>
+  <div class="box"></div>
+  <div class="box"></div>
+  <div class="box"></div>
+  <div class="box"></div>
+  <div class="box"></div>
+  <div class="box"></div>
+  <div class="box"></div>
+  <div class="box"></div>
+</div><div id="newGameButton">New Game</div>`;
+    const boardContainer = document.getElementById("boardContainer");
+    boardContainer.innerHTML = html2;
+    startGameButton.style.display = "none";
+  }
+  const newItemForm = document.getElementById("new-item");
   newItemForm.addEventListener("submit", submitName);
 }
 
-function submitName(event) {
-  event.preventDefault();
-  const playerName = document.getElementById("playerName");
-  playerName.innerText = newItemTextInput.value;
-}
+// function newGame() {}
 
-function startGame() {
-  const html2 = `<div id="newGameButton">New Game</div><div class="board">
-  <div class="box"></div>
-  <div class="box"></div>
-  <div class="box"></div>
-  <div class="box"></div>
-  <div class="box"></div>
-  <div class="box"></div>
-  <div class="box"></div>
-  <div class="box"></div>
-  <div class="box"></div>
-</div>`;
-  const boardContainer = document.getElementById("boardContainer");
-  boardContainer.innerHTML = html2;
-  startGameButton.style.display = "none";
-}
-
-function newGame() {}
-// newItemForm.addEventListener("submit", submitName);
 startGameButton.addEventListener("click", enterName);
 newGameButton.addEventListener("click", newGame);
