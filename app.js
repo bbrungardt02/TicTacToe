76fb866b1ab0c3bee4d15c3530cc796d02776325
bbrungardt02 <122824472+bbrungardt02@users.noreply.github.com>
2023-03-04 03:12:27 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
 const newGameButton = document.getElementById("newGameButton");
 const startGameButton = document.getElementById("startGameButton");
-const board = document.querySelector(".board")
+
+const html = `<div class="board">
+    <div class="box" id="0,0"></div>
+    <div class="box" id="0,1"></div>
+    <div class="box" id="0,2"></div>
+    <div class="box" id="1,0"></div>
+    <div class="box" id="1,1"></div>
+    <div class="box" id="1,2"></div>
+    <div class="box" id="2,0"></div>
+    <div class="box" id="2,1"></div>
+    <div class="box" id="2,2"></div>
+  </div><div id="newGameButton">New Game</div>`;
+
+const html1 = `
+  <ul id="human">
+  <li id="title2">Player:</li>
+  <li id="playerName"></li>
+  </ul>
+  <ul id="comp">
+  <li id="cpuTitle">Player:</li>
+  <li id="cpuName">Computer</li>
+  </ul>
+  <form id="new-item">
+    <input id="new-item-text" type="text" placeholder="Enter Player Name" required>
+    <button id="firstButton">Submit</button>
+  </form>
+  `;
+
+const html2 = `
+  <ul id="human2">
+  <li id="title2">Player:</li>
+  <li id="playerName"></li>
+  </ul>
+  <ul id="comp2">
+  <li id="player2Title">Player 2:</li>
+  <li id="player2Name"></li>
+  </ul>
+  <form id="new-item">
+    <input id="new-item-text" type="text" placeholder="Enter Player Name" required>
+    <button id="firstButton">Submit</button>
+  </form>
+  `;
 
 let gameState = {
   board: [
@@ -18,7 +59,7 @@ let gameState = {
 gameState.currentPlayer = gameState.players[0];
 
 function renderGame() {
-  gameBoard.innerText = gameState.board
+  board.innerText = gameState.board.value;
   // Call this function after you've changed your state values
   // Make references to DOM elements, and set the innerText,
   // or innerHTML to reflect our gameState.board
@@ -39,8 +80,8 @@ function switchPlayer() {
 
 function boardClick(event) {
   switchPlayer();
-  gameState.board.indexOf(event.target.value) = gameState.currentPlayer;
-  renderGame()
+  // gameState.board.indexOf(event.target.value) = gameState.currentPlayer;
+  renderGame(gameState.board);
   // Figure out how to get the coordinates off event object (e.target.value)
   // Use those coordinates to reference indexes in our gameState.board
   // Set the position in our board to the current player
@@ -49,41 +90,16 @@ function boardClick(event) {
 function runPlayerOne() {
   const pickPlayerAmtList = document.getElementById("pickPlayerAmtList");
   pickPlayerAmtList.style.display = "none";
-  const html = `
-  <ul id="human">
-  <li id="title2">Player:</li>
-  <li id="playerName"></li>
-  </ul>
-  <ul id="comp">
-  <li id="cpuTitle">Player:</li>
-  <li id="cpuName">Computer</li>
-  </ul>
-  <form id="new-item">
-    <input id="new-item-text" type="text" placeholder="Enter Player Name" required>
-    <button id="firstButton">Submit</button>
-  </form>
-  `;
   const enterPlayerName = document.getElementById("enterPlayerName");
-  enterPlayerName.innerHTML = html;
+  enterPlayerName.innerHTML = html1;
   function submitName(event) {
     event.preventDefault();
     const playerName = document.getElementById("playerName");
     const newItemTextInput = document.getElementById("new-item-text");
     playerName.innerText = newItemTextInput.value;
     newItemForm.style.display = "none";
-    const html2 = `<div class="board">
-    <div class="box" id="0,0"></div>
-    <div class="box" id="0,1"></div>
-    <div class="box" id="0,2"></div>
-    <div class="box" id="1,0"></div>
-    <div class="box" id="1,1"></div>
-    <div class="box" id="1,2"></div>
-    <div class="box" id="2,0"></div>
-    <div class="box" id="2,1"></div>
-    <div class="box" id="2,2"></div>
-  </div><div id="newGameButton">New Game</div>`;
     const boardContainer = document.getElementById("boardContainer");
-    boardContainer.innerHTML = html2;
+    boardContainer.innerHTML = html;
   }
   const newItemForm = document.getElementById("new-item");
   newItemForm.addEventListener("submit", submitName);
@@ -105,19 +121,8 @@ function runPlayerTwo2() {
     const newItemTextInput2 = document.getElementById("new-item-text2");
     player2Name.innerText = newItemTextInput2.value;
     newItemForm2.style.display = "none";
-    const html2 = `<div class="board">
-    <div class="box" id="0,0"></div>
-    <div class="box" id="0,1"></div>
-    <div class="box" id="0,2"></div>
-    <div class="box" id="1,0"></div>
-    <div class="box" id="1,1"></div>
-    <div class="box" id="1,2"></div>
-    <div class="box" id="2,0"></div>
-    <div class="box" id="2,1"></div>
-    <div class="box" id="2,2"></div>
-  </div><div id="newGameButton">New Game</div>`;
     const boardContainer = document.getElementById("boardContainer");
-    boardContainer.innerHTML = html2;
+    boardContainer.innerHTML = html;
   }
   const newItemForm2 = document.getElementById("new-item2");
   newItemForm2.addEventListener("submit", submitName2);
@@ -126,22 +131,8 @@ function runPlayerTwo2() {
 function runPlayerTwo() {
   const pickPlayerAmtList = document.getElementById("pickPlayerAmtList");
   pickPlayerAmtList.style.display = "none";
-  const html = `
-  <ul id="human2">
-  <li id="title2">Player:</li>
-  <li id="playerName"></li>
-  </ul>
-  <ul id="comp2">
-  <li id="player2Title">Player 2:</li>
-  <li id="player2Name"></li>
-  </ul>
-  <form id="new-item">
-    <input id="new-item-text" type="text" placeholder="Enter Player Name" required>
-    <button id="firstButton">Submit</button>
-  </form>
-  `;
   const enterPlayerName = document.getElementById("enterPlayerName");
-  enterPlayerName.innerHTML = html;
+  enterPlayerName.innerHTML = html2;
   function submitName(event) {
     event.preventDefault();
     const playerName = document.getElementById("playerName");
@@ -149,6 +140,8 @@ function runPlayerTwo() {
     playerName.innerText = newItemTextInput.value;
     newItemForm.style.display = "none";
     runPlayerTwo2();
+    const board = document.querySelector(".board");
+    board.addEventListener("click", boardClick);
   }
   const newItemForm = document.getElementById("new-item");
   newItemForm.addEventListener("submit", submitName);
@@ -182,4 +175,3 @@ function amtOfPlayers() {
 
 startGameButton.addEventListener("click", amtOfPlayers);
 // newGameButton.addEventListener("click", newGame);
-board.addEventListener("click", boardClick);
