9a904a07d7711bebc2d81e9c13fa5714c0361870
bbrungardt02 <122824472+bbrungardt02@users.noreply.github.com>
2023-03-18 00:39:31 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -87,9 +87,9 @@ function checkColumn() {
         board[0][j] === board[1][j] &&
         board[1][j] === board[2][j]
       ) {
-        if (board[i][0] === "x") {
+        if (board[0][j] === "x") {
           gameState.gameStatus = "xWins";
-        } else if (board[i][0] === "o") {
+        } else if (board[0][j] === "o") {
           gameState.gameStatus = "oWins";
         }
         console.log(board[0][j]);
@@ -102,16 +102,25 @@ function checkDiagonals() {
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board.length; j++) {
       if (
-        (board[0][0] !== null &&
-          board[0][0] === board[1][1] &&
-          board[1][1] === board[2][2]) ||
-        (board[0][2] !== null &&
-          board[0][2] === board[1][1] &&
-          board[1][1] === board[2][0])
+        board[0][0] !== null &&
+        board[0][0] === board[1][1] &&
+        board[1][1] === board[2][2]
       ) {
-        if (board[i][0] === "x") {
+        if (board[1][1] === "x") {
           gameState.gameStatus = "xWins";
-        } else if (board[i][0] === "o") {
+        } else if (board[1][1] === "o") {
+          gameState.gameStatus = "oWins";
+        }
+        console.log(board[1][1]);
+        return board[1][1];
+      } else if (
+        board[0][2] !== null &&
+        board[0][2] === board[1][1] &&
+        board[1][1] === board[2][0]
+      ) {
+        if (board[1][1] === "x") {
+          gameState.gameStatus = "xWins";
+        } else if (board[1][1] === "o") {
           gameState.gameStatus = "oWins";
         }
         console.log(board[1][1]);
